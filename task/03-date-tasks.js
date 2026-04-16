69d91eb0e2f921d3a50fa178b6f708b68a294552
Pavel Sinenka <sinenko.pavel72@gmail.com>
2026-04-16 21:37:09 +0200

    completed 03-date-tasks

--- task/03-date-tasks.js
+++ task/03-date-tasks.js
@@ -22,7 +22,7 @@
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
 function parseDataFromRfc2822(value) {
-   throw new Error('Not implemented');
+   return new Date(value);
 }
 
 /**
@@ -37,7 +37,7 @@ function parseDataFromRfc2822(value) {
  *    '2016-01-19T08:07:37Z' => Date()
  */
 function parseDataFromIso8601(value) {
-   throw new Error('Not implemented');
+   return new Date(value);
 }
 
 
@@ -56,7 +56,16 @@ function parseDataFromIso8601(value) {
  *    Date(2015,1,1)    => false
  */
 function isLeapYear(date) {
-   throw new Error('Not implemented');
+   let year = date.getFullYear();
+   if (year % 4 != 0) {
+      return false;
+   } else if (year % 100 != 0) {
+      return true;
+   } else if (year % 400 != 0) {
+      return false;
+   } else {
+      return true;
+   }
 }
 
 
@@ -76,7 +85,15 @@ function isLeapYear(date) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
 function timeSpanToString(startDate, endDate) {
-   throw new Error('Not implemented');
+   let diff = endDate.getTime() - startDate.getTime();
+   let hours = Math.floor(diff / 3600000);
+   let min = Math.floor((diff % 3600000) / 60000);
+   let sec = Math.floor((diff % 60000) / 1000);
+   let ms = diff % 1000;
+   return ("0" + hours).slice(-2) + ":" + 
+          ("0" + min).slice(-2) + ":" + 
+          ("0" + sec).slice(-2) + "." + 
+          ("00" + ms).slice(-3);
 }
 
 
@@ -94,7 +111,12 @@ function timeSpanToString(startDate, endDate) {
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
 function angleBetweenClockHands(date) {
-    throw new Error('Not implemented');
+    let hours = date.getUTCHours() % 12; 
+    let min = date.getUTCMinutes(); 
+    let angle = Math.abs (0.5 * (60 * hours - 11 * min)); 
+    if (angle > 180) angle = 360 - angle; 
+
+    return angle * Math.PI / 180; 
 }
 
 
