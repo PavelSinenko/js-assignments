869dde5196d6554da0457b0d66faf448a0201fee
Pavel Sinenka <sinenko.pavel72@gmail.com>
2026-05-09 18:40:39 +0300

    did 3 of 4 tasks in 08-objects

--- task/08-objects-tasks.js
+++ task/08-objects-tasks.js
@@ -23,8 +23,13 @@
  *    console.log(r.getArea());   // => 200
  */
 function Rectangle(width, height) {
-    throw new Error('Not implemented');
+    this.width = width;
+    this.height = height;
 }
+
+Rectangle.prototype.getArea = function() {
+    return this.width * this.height;
+};
 
 
 /**
@@ -38,7 +43,8 @@ function Rectangle(width, height) {
  *    { width: 10, height : 20 } => '{"height":10,"width":20}'
  */
 function getJSON(obj) {
-    throw new Error('Not implemented');
+    let str = JSON.stringify(obj);
+    return str;
 }
 
 
@@ -54,7 +60,10 @@ function getJSON(obj) {
  *
  */
 function fromJSON(proto, json) {
-    throw new Error('Not implemented');
+    let newStr = JSON.parse(json);
+    let obj = Object.create(proto);
+    Object.assign(obj, newStr);
+    return obj;
 }
 
 
